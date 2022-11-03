4f6d85f6f8c26c852c08b7d8e1ce031af046917b
Av-sek <abhishekghorashainee07@gmail.com>
2022-11-03 19:06:02 +0545

    user login fix

--- django_server/shopping_cart/serializers.py
+++ django_server/shopping_cart/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 from product.serializers import ProductSerializer
+from api.serializers import UserSerializer
 from product.models import Product
 from .models import ShoppingCart
 
 
 
 class ShoppingCartSeralizer(serializers.ModelSerializer):
-    product = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    product_data = ProductSerializer(source='product',read_only=True)
+    user_data = UserSerializer(source='user',read_only=True)
     class Meta:
         model = ShoppingCart
         fields = [
             'id',
             'product',
+            'product_data',
             'user',
+            'user_data',
             'quantity',
             ]
+        
+    def create(self,validated_data):
+        user = self.request.user
+        cart = ShoppingCart.objects.create(user=user,**validated_data)
+        return cart
--- django_server/shopping_cart/views.py
+++ django_server/shopping_cart/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication 
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import ShoppingCart
 from .serializers import ShoppingCartSeralizer
 from rest_framework.response import Response
@@ -8,9 +11,18 @@ from rest_framework.response import Response
 
 
 class ShoppingCartView(APIView):
-    
+    authentication_classes = [JWTAuthentication,SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     
     def get(self, request):
-        cart = ShoppingCart.objects.all()
+        print(request.user)
+        cart = ShoppingCart.objects.filter(user=request.user)
         serializer = ShoppingCartSeralizer(cart, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ShoppingCartSeralizer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
