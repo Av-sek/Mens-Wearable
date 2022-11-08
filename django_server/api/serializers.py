b1e00d1479eb04142faa08b8459c4ad72aadea78
Av-sek <abhishekghorashainee07@gmail.com>
2022-11-08 19:00:38 +0545

    roles

--- django_server/api/serializers.py
+++ django_server/api/serializers.py
@@ -3,14 +3,25 @@ from operator import mod
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from django.core import validators
-
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields=['username','email','password','is_admin','is_user']
+        fields=['username','email','password','is_staff']
         extra_kwargs = {'password':{'write_only':True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         user.set_password(validated_data['password'])
         return user
+    
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data.update(
+            {
+                'is_staff': self.user.is_staff,
+            }
+        )
+        return data
--- django_server/api/urls.py
+++ django_server/api/urls.py
@@ -3,14 +3,10 @@ from django.urls import re_path,path
 from api.views import UserCreate
 from .viewsets import *
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
+from .views import *
 urlpatterns =[
     path("",UserCreate.as_view(), name="create_user"),
-    re_path(r'^token/?$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    re_path(r'^token/?$', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path(r'^api/token/verify/?$', TokenVerifyView.as_view(), name='token_verify'),
     re_path(r'^token/refresh/?$', TokenRefreshView.as_view(), name='token_refresh'),
 ]
